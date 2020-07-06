94b33013c73446ef2bd07aac7e635065239562f5
hbk671104 <hbk671104@gmail.com>
2020-07-06 21:03:17 +0800

    code2Session

--- cloud.js
+++ cloud.js
@@ -5,13 +5,11 @@ const path = require('path')
 /**
  * 加载 functions 目录下所有的云函数
  */
-fs.readdirSync(path.join(__dirname, 'functions')).forEach( file => {
+fs.readdirSync(path.join(__dirname, 'functions')).forEach(file => {
   require(path.join(__dirname, 'functions', file))
 })
 
-/**
- * 一个简单的云代码方法
- */
-AV.Cloud.define('hello', function(request) {
+// 微信登录凭证校验
+AV.Cloud.define('code2Session', async request => {
   return 'Hello world!'
 })
